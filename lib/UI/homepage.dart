663c8aed27da1131eda6e1df64f51c7ae35f15fc
moses <mosesopiyo54@gmail.com>
2022-05-18 12:30:49 +0300

    addition of homepage base file

---
+++ lib/UI/homepage.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: unused_import, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:gateway/UI/components/homepage_body.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: buildAppBar(), body: Body());
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(
+          Icons.sort,
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        Container(
+          margin: EdgeInsetsDirectional.only(end: 12),
+          decoration:
+              BoxDecoration(borderRadius: BorderRadiusDirectional.circular(10)),
+          child: IconButton(
+            icon: Icon(
+              Icons.notifications,
+              size: 30.0,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+}
