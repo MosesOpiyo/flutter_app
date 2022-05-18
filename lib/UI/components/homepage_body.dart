e62780dbd57b664f1f8b3b0cd4fd07f0dc330686
moses <mosesopiyo54@gmail.com>
2022-05-18 13:08:08 +0300

    addition of search field

--- lib/UI/components/homepage_body.dart
+++ lib/UI/components/homepage_body.dart
@@ -14,7 +14,7 @@ class Body extends StatelessWidget {
         Column(
           children: <Widget>[
             Container(
-              height: size.height * 0.25,
+              height: size.height * 0.32,
               color: Colors.amber,
               child: Column(
                 children: <Widget>[
@@ -76,12 +76,50 @@ class Body extends StatelessWidget {
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  Container(
+                    padding: EdgeInsetsDirectional.only(end: 170, top: 15),
+                    child: Text(
+                      "Hello user,",
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsetsDirectional.only(end: 120, top: 5),
+                    child: Text(
+                      "Where to?",
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsetsDirectional.only(top: 10),
+                    width: 330,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromARGB(255, 244, 208, 99),
+                          border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2.0),
+                              borderRadius: BorderRadius.circular(20.0)),
+                          hintText: 'Search area you are going',
+                          hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      keyboardType: TextInputType.text,
+                    ),
+                  ),
                 ],
               ),
             )
           ],
-        )
+        ),
       ],
     );
   }
