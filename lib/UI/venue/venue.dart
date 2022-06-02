1df044ed5786818faf1edb9a0c2d705d9fa7151d
moses <mosesopiyo54@gmail.com>
2022-06-02 07:17:17 +0300

    image editing to fit top cover

--- lib/UI/venue/venue.dart
+++ lib/UI/venue/venue.dart
@@ -10,38 +10,85 @@ class Venue extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildVenueAppBar(),
-      body: VenueBody(),
-    );
-  }
-
-  AppBar buildVenueAppBar() {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      leading: IconButton(
-        icon: Icon(
-          Icons.chevron_left,
-          color: Colors.black,
-          size: 30,
-        ),
-        onPressed: () {},
-      ),
-      actions: [
-        Container(
-          padding: EdgeInsets.all(10),
-          decoration:
-              BoxDecoration(borderRadius: BorderRadiusDirectional.circular(10)),
-          child: IconButton(
-            icon: Icon(
-              Icons.more_vert,
-              size: 25.0,
-              color: Colors.black,
-            ),
-            onPressed: () {},
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(270),
+        child: Container(
+          height: 300,
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                  child: ClipRRect(
+                child: Image(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      'https://images.pexels.com/photos/5672376/pexels-photo-5672376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                    )),
+              )),
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 25),
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.chevron_left,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {}),
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Container(
+                  height: 120,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Colors.black.withOpacity(0.7),
+                          Colors.transparent
+                        ]),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                left: 15,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsetsDirectional.only(top: 5, end: 55),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.location_pin,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 5),
+                              child: Text(
+                                'Location',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ],
+                        )),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-      ],
+      ),
+      body: VenueBody(),
     );
   }
 }
